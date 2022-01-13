97f21a75f53d4b13ddabeea32da1328c1c756f43
Paulo Moutinho <paulo_vicali@icloud.com>
2022-01-13 02:37:30 -0300

    aula sobre variáveis

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+let name = 'Paulo'
+let job = 'Líder'
+let workspace = 'Igreja'
+
+let yearsOfWork = 3
+
+// guardando uma arrow function dentro de uma variável
+const allVariables = () => console.log(`${name} é ${job} em uma ${workspace} á ${yearsOfWork} anos.`)
+
+allVariables()
